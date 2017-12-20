d5ef0f41148c89e9b0289a897d24cbe40b94807b
Draven <zhangslob@gmail.com>
2017-12-20 10:58:08 +0800

    Update settings.py

--- taobao_info/taobao_info/settings.py
+++ taobao_info/taobao_info/settings.py
@@ -30,7 +30,7 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
